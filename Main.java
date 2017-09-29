788d267813355e39ea736851e8e8c758a6a4f5a8
goodguytim <32111143+goodguytim@users.noreply.github.com>
2017-09-29 15:39:16 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Oh boy do I love writing some good old code!");
+	// write your code here
+    }
+}
